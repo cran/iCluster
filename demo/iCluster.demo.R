29d70e4ede05fd35eae4cf1ef15280cb4365f85e
Ronglai Shen <shenr@mskcc.org>
2012-05-01 00:00:00 +0000

    version 2.1.0

--- demo/iCluster.demo.R
+++ demo/iCluster.demo.R
@@ -1,29 +1,47 @@
-############################################
-#This is an example of iCluster implementation 
-#using the Pollack study breast cancer data  
-#set. DNA and mRNA data measured in 37 primary 
-#tumors and four cell lines on chromosome 17 
-#are included.
-#last update: 11/07/09
-############################################
+setwd("/home/nfs/shenr/iClusterPLoSONEsoftware/")
+library(iCluster)
 
-#load("/home/nfs/moq/Gdrive/iCluster/iCluster/data/breast.chr17.Rdata")
+data(breast.chr17)
+fit=iCluster(datasets=breast.chr17, k=4, lambda=c(0.2,0.2))
 
-data("breast.chr17.Rdata")
-#source("iCluster.R")
- #setting the penalty parameter lambda=0 returns non-sparse fit
-fit=iCluster(datasets, k=4, lambda=0.2, max.iter=50)
+plotiCluster(fit)
 
-#source("plot.separability.R")
-plot.iCluster(fit=fit, label=rownames(datasets[[2]]))
+#a simple data transformation for nice image contrast
+cn=breast.chr17[[2]]
+cn[cn< -1.5]= -1.5;cn[cn>1.5]=1.5
+exp=breast.chr17[[1]]
+exp[exp< -2.5]= -2.5; exp[exp>2.5]=2.5
 
-#source("compute.pod.R")
+plotHeatmap(datasets=list(cn,exp),fit=fit)
+
+#GBM data example including three data matrices: copy number, methylation, and expression.
+data(gbm) 
+data(coord)
+chr=coord[,1]
+
+#A variant of iCluster method with variance weighted shrinkage
+fit=iCluster2(datasets=gbm, k=3, lambda=list(0.44,0.33,0.28))
+             
+plotiCluster(fit=fit)
+
 compute.pod(fit)
 
+plotHeatmap(fit=fit, datasets=gbm, feature.order=c(FALSE,TRUE,TRUE), sparse=c(FALSE,TRUE,TRUE),plot.chr=c(TRUE,FALSE,FALSE), chr=chr)
+
+#Model tuning (using a simulated data example)
+data(simu.datasets)
+cv.fit=alist()
+for(k in 2:5){
+  cat(paste("K=",k,sep=""),'\n')
+  cv.fit[[k]]=tune.iCluster2(datasets=simu.datasets, k,nrep=2, n.lambda=8)
+}
+
+#Reproducibility index (RI) plot
+plotRI(cv.fit)
 
 
- 
-
-                                               
-
+#Based on the RI plot, k=3 is the best solution
+best.fit=cv.fit[[3]]$best.fit
+#Try different color schemes
+plotHeatmap(fit=best.fit,datasets=simu.datasets,sparse=c(TRUE,TRUE),col.scheme=list(bluered(256), greenred(256)))
 
